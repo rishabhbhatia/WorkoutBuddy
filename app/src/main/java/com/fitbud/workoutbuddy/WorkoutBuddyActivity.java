28bdeb4fc2ebbaa6e25772a66742e4138c408876
Rishabh Bhatia <rishabh.bhatia08@gmail.com>
2016-05-21 19:06:50 +0530

    First commit, Setting up libraries to project

---
+++ app/src/main/java/com/fitbud/workoutbuddy/WorkoutBuddyActivity.java
@@ -0,0 +1,12 @@
+package com.fitbud.workoutbuddy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WorkoutBuddyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
