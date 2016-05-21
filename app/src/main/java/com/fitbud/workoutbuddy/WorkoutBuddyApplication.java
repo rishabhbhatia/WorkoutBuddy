b0e2414bc5a96e68192cc765b2978059d0a6c550
Rishabh Bhatia <rishabh.bhatia08@gmail.com>
2016-05-21 23:31:35 +0530

    Workout buddy application file

---
+++ app/src/main/java/com/fitbud/workoutbuddy/WorkoutBuddyApplication.java
@@ -0,0 +1,15 @@
+package com.fitbud.workoutbuddy;
+
+import android.app.Application;
+
+/**
+ * Created by Rishabh Bhatia on 5/21/2016.
+ */
+public class WorkoutBuddyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
