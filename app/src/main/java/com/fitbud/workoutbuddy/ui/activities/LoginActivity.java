0ae421600c6db7a54c68c49439415d06a00e4ba7
Rishabh Bhatia <rishabh.bhatia08@gmail.com>
2016-05-22 19:06:59 +0530

    Login Screen

--- app/src/main/java/com/fitbud/workoutbuddy/ui/activities/LoginActivity.java
+++ app/src/main/java/com/fitbud/workoutbuddy/ui/activities/LoginActivity.java
@@ -1,7 +1,85 @@
 package com.fitbud.workoutbuddy.ui.activities;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.fitbud.workoutbuddy.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 /**
  * Created by Rishabh Bhatia on 5/22/2016.
  */
 public class LoginActivity extends WorkoutBuddyActivity {
+
+    @BindView(R.id.bt_login)
+    Button btLogin;
+    @BindView(R.id.et_login_pass)
+    EditText etPass;
+    @BindView(R.id.et_login_username)
+    EditText etUsername;
+    @BindView(R.id.ll_login_username_holder)
+    LinearLayout linearUsernameHolder;
+    @BindView(R.id.ll_login_pass_holder)
+    LinearLayout linearPasswordHolder;
+    @BindView(R.id.iv_login_username_error)
+    ImageView ivUsernameError;
+    @BindView(R.id.iv_login_password_error)
+    ImageView ivPasswordError;
+
+    private Animation shake;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+
+        measureLoginButton();
+
+        shake = AnimationUtils.loadAnimation(this, R.anim.shake);
+    }
+
+    private void measureLoginButton()
+    {
+        linearUsernameHolder.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+        etUsername.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+
+        linearPasswordHolder.setMinimumWidth(linearUsernameHolder.getMeasuredWidth());
+        linearPasswordHolder.setMinimumHeight(linearUsernameHolder.getMeasuredHeight());
+
+        etPass.setWidth(etUsername.getMeasuredWidth());
+        etPass.setHeight(etUsername.getMeasuredHeight());
+
+        btLogin.setWidth(linearUsernameHolder.getMeasuredWidth());
+        btLogin.setHeight(linearUsernameHolder.getMeasuredHeight());
+    }
+
+    @OnClick(R.id.bt_login)
+    public void onLogin()
+    {
+        if(TextUtils.isEmpty(etUsername.getText()))
+        {
+            ivUsernameError.startAnimation(shake);
+        }else if(TextUtils.isEmpty(etPass.getText()))
+        {
+            ivPasswordError.startAnimation(shake);
+        }else
+        {
+            Intent intent = new Intent(LoginActivity.this, MainLandingActivity.class);
+            startActivity(intent);
+            overridePendingTransition(R.anim.slide_in_from_bottom, R.anim.slide_out_to_bottom);
+        }
+    }
 }
---
+++ app/src/main/java/com/fitbud/workoutbuddy/ui/activities/MainLandingActivity.java
@@ -0,0 +1,15 @@
+package com.fitbud.workoutbuddy.ui.activities;
+
+import android.os.Bundle;
+
+/**
+ * Created by Rishabh Bhatia on 5/22/2016.
+ */
+public class MainLandingActivity extends WorkoutBuddyActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
