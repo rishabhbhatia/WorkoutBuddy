450f7481da0129561e56f35f1701c6f569e37c55
Rishabh Bhatia <rishabh.bhatia08@gmail.com>
2016-05-22 17:12:23 +0530

    Splash Screen

---
+++ app/src/main/java/com/fitbud/workoutbuddy/ui/activities/LoginActivity.java
@@ -0,0 +1,7 @@
+package com.fitbud.workoutbuddy.ui.activities;
+
+/**
+ * Created by Rishabh Bhatia on 5/22/2016.
+ */
+public class LoginActivity extends WorkoutBuddyActivity {
+}
--- app/src/main/java/com/fitbud/workoutbuddy/ui/activities/SplashActivity.java
+++ app/src/main/java/com/fitbud/workoutbuddy/ui/activities/SplashActivity.java
@@ -1,15 +1,60 @@
 package com.fitbud.workoutbuddy.ui.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.fitbud.workoutbuddy.R;
 
 /**
  * Created by Rishabh Bhatia on 5/22/2016.
  */
 public class SplashActivity extends WorkoutBuddyActivity {
 
+    private Handler handler = null;
+    private Runnable runnable = null;
+    private final int SPLASH_TIME = 2000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+        setContentView(R.layout.activity_splash);
     }
+
+    @Override
+    protected void onPause() {
+
+        if(handler != null && runnable != null)
+        {
+            handler.removeCallbacks(runnable);
+            handler = null;
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(handler == null)
+        {
+            handler = new Handler();
+
+            runnable = new Runnable() {
+                @Override
+                public void run() {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                            startActivity(intent);
+                            overridePendingTransition(R.anim.slide_in_from_bottom, R.anim.slide_out_to_bottom);
+                        }
+                    });
+                }
+            };
+
+            handler.postDelayed(runnable,SPLASH_TIME);
+        }
+    }
+
 }
