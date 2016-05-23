307ab58d2dbaab20eb6f933fc953717c82fd1fbd
Rishabh Bhatia <rishabh.bhatia08@gmail.com>
2016-05-23 23:43:48 +0530

    Login Screen back button handling

--- app/src/main/java/com/fitbud/workoutbuddy/ui/activities/LoginActivity.java
+++ app/src/main/java/com/fitbud/workoutbuddy/ui/activities/LoginActivity.java
@@ -66,6 +66,15 @@ public class LoginActivity extends WorkoutBuddyActivity {
         btLogin.setHeight(linearUsernameHolder.getMeasuredHeight());
     }
 
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
+    }
+
     @OnClick(R.id.bt_login)
     public void onLogin()
     {
