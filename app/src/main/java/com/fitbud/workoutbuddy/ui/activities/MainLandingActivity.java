452f062c85eac1e776d00dfcd02f852cfc756dcf
Rishabh Bhatia <rishabh.bhatia08@gmail.com>
2016-05-24 22:53:30 +0530

    Landing Screen set layout

--- app/src/main/java/com/fitbud/workoutbuddy/ui/activities/MainLandingActivity.java
+++ app/src/main/java/com/fitbud/workoutbuddy/ui/activities/MainLandingActivity.java
@@ -3,6 +3,8 @@ package com.fitbud.workoutbuddy.ui.activities;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.fitbud.workoutbuddy.R;
+
 /**
  * Created by Rishabh Bhatia on 5/22/2016.
  */
@@ -11,7 +13,7 @@ public class MainLandingActivity extends WorkoutBuddyActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_main_landing);
     }
 
     @Override
