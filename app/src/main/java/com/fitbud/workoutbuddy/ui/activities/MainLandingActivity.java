5ffe81165897374b2f6ddf77e579fefa0b05eae7
Rishabh Bhatia <rishabh.bhatia08@gmail.com>
2016-05-28 22:13:16 +0530

    Replace support recyclerview with Super Recyclerview for main landing screen

--- app/src/main/java/com/fitbud/workoutbuddy/ui/activities/MainLandingActivity.java
+++ app/src/main/java/com/fitbud/workoutbuddy/ui/activities/MainLandingActivity.java
@@ -2,18 +2,39 @@ package com.fitbud.workoutbuddy.ui.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import com.fitbud.workoutbuddy.R;
+import com.malinskiy.superrecyclerview.SuperRecyclerView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Rishabh Bhatia on 5/22/2016.
  */
 public class MainLandingActivity extends WorkoutBuddyActivity {
 
+    @BindView(R.id.rv_landing_clients)
+    SuperRecyclerView rvClients;
+    @BindView(R.id.rv_landing_recently_viewed)
+    SuperRecyclerView rvRecentlyViewed;
+    @BindView(R.id.ll_landing_new_workout_footer)
+    LinearLayout linearNewWorkoutFooter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_landing);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.ll_landing_new_workout_footer)
+    public void loadNewWorkoutScreen() {
+
     }
 
     @Override
